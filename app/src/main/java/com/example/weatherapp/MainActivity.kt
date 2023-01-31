27451c8f59e8059b8e76cb0082170dc5ed683750
merajuni7ed <merajmusavi1243m@gmail.com>
2023-01-31 15:08:22 +0330

    get more data about situation of weather and bind it into application

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         val client = OkHttpClient()
 
         val req = Request.Builder()
-            .url("https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=0ae333a163d630d5372038e78ad370dc")
+            .url("https://api.openweathermap.org/data/2.5/weather?q=manchester&appid=0ae333a163d630d5372038e78ad370dc&unit=metric")
             .build()
         client.newCall(req).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
@@ -37,27 +37,55 @@ class MainActivity : AppCompatActivity() {
                 val icon_id = obj_weather.getString("icon")
                 val sunrise = jo.getJSONObject("sys").getInt("sunrise")
                 val sunset = jo.getJSONObject("sys").getInt("sunset")
-
                 val url_icon = "https://openweathermap.org/img/wn/${icon_id}@2x.png"
                 val StatusDescription = obj_weather.getString("description")
+                val weather_detail = jo.getJSONObject("main")
+                val temp_min = weather_detail.getDouble("temp_min")
+                val temp_max = weather_detail.getDouble("temp_min")
+                val feels_like = weather_detail.getDouble("feels_like")
+                val pressure = weather_detail.getInt("pressure")
+
 
                 runOnUiThread {
-                    ShowName(jo.getString("name"), StatusDescription,url_icon,sunrise,sunset)
-                } 
+                    ShowName(
+                        jo.getString("name"),
+                        StatusDescription,
+                        url_icon,
+                        sunrise,
+                        sunset,
+                        feels_like,
+                        temp_min, temp_max, pressure
+                    )
+                }
             }
 
         })
     }
 
-    fun ShowName(cityName: String, description: String,imageUrl:String,sunrise:Int,sunset:Int) {
+    fun ShowName(
+        cityName: String,
+        description: String,
+        imageUrl: String,
+        sunrise: Int,
+        sunset: Int,
+        feels_like: Double,
+        temp_min: Double,
+        temp_max: Double,
+        pressure: Int
+    ) {
         binding.tvCityName.text = cityName
         binding.description.text = description
-        binding.tvSunrise.text = showTimeBasedOnUnixTime(sunrise).toString()
-        binding.tvSunset.text = showTimeBasedOnUnixTime(sunset).toString()
+        binding.tvSunrise.text = "sunrise : " + showTimeBasedOnUnixTime(sunrise).toString()
+        binding.tvSunset.text = "sunset : " + showTimeBasedOnUnixTime(sunset).toString()
+        binding.tvFeelLike.text = feels_like.toString()
+        binding.tvPressure.text = pressure.toString()
+        binding.tvTempMax.text = temp_max.toString()
+        binding.tvTempMin.text = temp_min.toString()
         Glide.with(this).load(imageUrl).into(binding.ivWeather)
     }
-    fun showTimeBasedOnUnixTime(unixtime:Int):String{
-        val time = unixtime*1000.toLong()
+
+    fun showTimeBasedOnUnixTime(unixtime: Int): String {
+        val time = unixtime * 1000.toLong()
         var time_converter = Date(time)
 
         val formater = SimpleDateFormat("HH:MM a")
